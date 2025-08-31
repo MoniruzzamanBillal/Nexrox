4accb5e0588090937b19ea49e1b5dae77aab044e
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-08-31 15:47:56 +0600

    auth check middleware added

---
+++ server/src/app/middleware/authCheck.ts
@@ -0,0 +1,35 @@
+import httpStatus from "http-status";
+import Jwt, { JwtPayload } from "jsonwebtoken";
+import AppError from "../Error/AppError";
+import config from "../config";
+import catchAsync from "../util/catchAsync";
+
+const authCheck = catchAsync(async (req, res, next) => {
+  const header = req.headers.authorization;
+
+  if (!header) {
+    return next(
+      new AppError(
+        httpStatus.UNAUTHORIZED,
+        "Authorization header missing or malformed"
+      )
+    );
+  }
+
+  const token = header.split(" ")[1];
+
+  let decoded: JwtPayload;
+
+  try {
+    decoded = Jwt.verify(token, config.jwt_secret as string) as JwtPayload;
+  } catch (error) {
+    return next(
+      new AppError(httpStatus.UNAUTHORIZED, "Invalid or expired token")
+    );
+  }
+
+  req.user = decoded;
+  next();
+});
+
+export default authCheck;
